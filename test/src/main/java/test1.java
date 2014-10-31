fabfef61d32114760fe8c6aac65fea802bd18cb7
jiangwentao <jiangwt@inspur.com>
2014-10-31 13:51:45 +0800

    commit the file to identify branch

---
+++ test/src/main/java/test1.java
@@ -0,0 +1,15 @@
+
+//import com.jiangwentao.mvnbook.helloworld.HelloWorld;
+
+public class test1
+{
+    public String sayHello()
+    {
+        return "Hello Maven";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print( new test1().sayHello() );
+    }
+}
