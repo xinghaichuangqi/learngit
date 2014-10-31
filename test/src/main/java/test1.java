ed9f20d9a491c554a6b7cdfb49419aef46ed4e43
jiangwentao <jiangwt@inspur.com>
2014-10-31 13:59:52 +0800

    commit modify test1.java

--- test/src/main/java/test1.java
+++ test/src/main/java/test1.java
@@ -5,11 +5,12 @@ public class test1
 {
     public String sayHello()
     {
-        return "Hello Maven";
+        return "Hello Maven,hello inspur";
     }
 
     public static void main(String[] args)
     {
         System.out.print( new test1().sayHello() );
+        System.out.println("test");
     }
 }
